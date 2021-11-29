04ce3ae426bc106d424b41c194b8d37e77755383
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-29 03:10:44 -0500

    Mostrar mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core'
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
   styles: []
 })
-export class MapaComponent implements OnInit {
+export class MapaComponent implements AfterViewInit {
+  @ViewChild('map', { static: true }) mapaElement!: ElementRef
+  map!: google.maps.Map
+
   // constructor () { }
 
-  ngOnInit (): void {
+  ngAfterViewInit (): void {
+    this.cargarMapa()
+  }
+
+  cargarMapa (): any {
+    const latlng = new google.maps.LatLng(37.784679, -122.395936)
+    const mapOpciones: google.maps.MapOptions = {
+      center: latlng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapaElement.nativeElement, mapOpciones)
   }
 }
