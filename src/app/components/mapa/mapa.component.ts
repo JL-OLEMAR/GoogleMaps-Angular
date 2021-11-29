3d2d52820d38866b02e6ff2578a6c8a826a2fa01
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-29 01:08:20 -0500

    add inicio del proyecto

---
+++ src/app/components/mapa/mapa.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styles: []
+})
+export class MapaComponent implements OnInit {
+  // constructor () { }
+
+  ngOnInit (): void {
+  }
+}
